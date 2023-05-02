d726661e3b1de9750163cf083ce4955c8dbd17f4
HRISHI313 <abishekbanerjee10@gmail.com>
2023-05-02 15:08:38 +0530

    d

--- src/TrainingPrediction_components/data_ingestion.py
+++ src/TrainingPrediction_components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.exception import CustomException
 from src.logger import logging
 from src.utils import read_mongodb
 
+from sklearn.model_selection import train_test_split
+
 import pandas as pd
 from dataclasses import dataclass
 
@@ -21,15 +23,25 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info('Entered into the Data ingestion method')
         try:
-            train_df = read_mongodb('user2023','test543', 'train_data')
-            test_df = read_mongodb('user2023','test543', 'test_data')
+            data1 = read_mongodb('banerjeeabishek999','mongodb', 'data')
+            data2 = read_mongodb('banerjeeabishek999','mongodb', 'phishy_data')
             logging.info('Read Dataset from mongodb as dataframe')
+
+            logging.info('Data concatenating and shuffling initiated')
+            data = pd.concat([data1,data2],axis = 0)
+            data.sample(frac = 1)
+            logging.info('Data concatenating and shuffling finished')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
-            train_df.to_csv(self.ingestion_config.train_data_path, header=True, index=False)
-            test_df.to_csv(self.ingestion_config.test_data_path,header=True, index=False)
-            logging.info('Ingestion of data is completed')
+            logging.info("train test completed initiated")
+            train_data,test_data = train_test_split(data,test_size = 0.3,random_state = 42)
+
+            train_data.to_csv(self.ingestion_config.train_data_path,index = False,header = True)
+            test_data.to_csv(self.ingestion_config.test_data_path,index = False,header = True)
+            logging.info("train test split completed")
+
+            logging.info("Data ingestion completed")
 
             return (
                 self.ingestion_config.train_data_path,
--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,8 @@ import pymongo
 import pandas as pd
 
 def read_mongodb(user,password,collection):
-    client1 = pymongo.MongoClient("mongodb+srv://{}:{}@cluster0.k5pminz.mongodb.net/?retryWrites=true&w=majority".format(user,password))
-    db = client1['phishing_classification']
+    client1 = pymongo.MongoClient("mongodb+srv://{}:{}@cluster0.numsybe.mongodb.net/?retryWrites=true&w=majority".format(user,password))
+    db = client1['Phishing_classifier']
     cursor = db['{}'.format(collection)].find()
     dataframe = pd.DataFrame(list(cursor))
     dataframe.drop(columns=['_id'],axis=1,inplace=True)
